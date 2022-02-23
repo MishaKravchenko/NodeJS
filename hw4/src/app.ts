ab42b9154842717bea83e254ef138e3996a695ab
MishaKravchenko <mishakravchenko.1998@gmail.com>
2022-02-24 00:51:10 +0200

    TS.eslint.typeorm.migrations

---
+++ hw4/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log(2);
+});
+
+app.listen(5500, () => {
+    console.log('Server has started!!!');
+});
